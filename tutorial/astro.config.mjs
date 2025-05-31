ba7b4cd4a9a29ffa8096aa999b21371bc3a3f36b
Tristan Joshua Alba <tristanjoshuaalba@192.168.0.37>
2025-05-31 17:12:52 +0800

    Correcting config

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tristanjoshuaalba.github.io',
-    base: '/tristanalba-portfolio//tutorial'
+    base: '/tristanalba-portfolio'
 });
